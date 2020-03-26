cee486bbd0db67bebd0fee5912b1a0e4a649f6c1
Ali Hassan <20568079+ali2210@users.noreply.github.com>
2020-03-26 22:45:59 +0500

    V-0.01 initial repo

---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+
+
+import(
+	 "fmt"
+	"net/http"
+	"github.com/gorilla/mux"
+	"html/template"	
+	"log"
+)
+
+
+func main(){
+	routing := mux.NewRouter()
+
+	routing.HandleFunc("/{title}/home", Home)
+	routing.HandleFunc("/{title}/signup", NewUser)
+	routing.HandleFunc("/{title}/login", Existing)
+	routing.HandleFunc("/dummy", Dump)
+
+	log.Println("Listening at 9101 ... please wait...")
+	http.ListenAndServe(":9101",routing)
+
+
+}
+
+func Home(w http.ResponseWriter, r *http.Request){
+	temp := template.Must(template.ParseFiles("index.html"))
+	if r.Method  ==  "GET"{
+		fmt.Println("Method:" + r.Method)
+		temp.Execute(w,"Home")
+	}else{
+		temp := template.Must(template.ParseFiles("dump.html"))
+		    r.ParseForm()
+			fmt.Println("Url:", r.URL.Path)
+			fmt.Println("Method:" + r.Method)
+			
+			// FILE Upload ....
+			file := UploadFiles(r); if file != "blabla"{
+				print(file)
+			}else{
+				print("size must be less than 5KB")
+			}
+			
+		temp.Execute(w,"Dump")
+
+	}
+
+}
+
+func NewUser (w http.ResponseWriter, r *http.Request){
+	temp := template.Must(template.ParseFiles("register.html"))
+	temp.Execute(w,"Regsiter")
+}
+
+func Existing(w http.ResponseWriter, r *http.Request){
+	temp := template.Must(template.ParseFiles("login.html"))
+	temp.Execute(w,"Login")
+}
+
+
+func Dump(w http.ResponseWriter, r *http.Request){
+	temp := template.Must(template.ParseFiles("dump.html"))
+	temp.Execute(w,"Dump")
+}
+
+func UploadFiles(r *http.Request)(string){
+	r.ParseMultipartForm(10 << 50)
+			file , handler, err := r.FormFile("fileSeq"); if err != nil{
+				fmt.Println("Error failed.... retry",err)
+				return handler.Filename
+			}
+			defer file.Close()
+				if(handler.Size <= (50 * 1024)){
+					fmt.Println("File name:" + handler.Filename)
+					return handler.Filename
+				}
+				return "blabla"
+			
+}
