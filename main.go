35dd547668973be8d2c5a6c7a3407b4eb9d6c937
Ali2210 <alideveloper95@gmail.com>
2020-03-30 12:38:30 +0500

    V-0.02 sequence-file-data

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 
 
 import(
-	 "fmt"
+	"fmt"
 	"net/http"
 	"github.com/gorilla/mux"
 	"html/template"	
 	"log"
+	"io/ioutil"
+	"os"
 )
 
 	type Response struct{
@@ -40,19 +42,30 @@ func Home(w http.ResponseWriter, r *http.Request){
 		    r.ParseForm()
 			fmt.Println("Url:", r.URL.Path)
 			fmt.Println("Method:" + r.Method)
+			
+
 			// FILE Upload ....
-			file := UploadFiles(r); if file != "blabla"{
-				println(file)
+			file := UploadFiles(r); if file != nil{
+				println(file) // user file upload
+				choose := r.FormValue("choose")
+				print("choose I make:", choose)
+				switch choose{
+				case "0":
+					fmt.Fprintf(w, "Please choose any option ...")
+				case "1":
+					 FileReadFromDisk();
+
+					// // File , err := os.Stat(); if err != nil{
+					// // 	println("File unable to open", err)
+					// // }
+					// // println("File :" , File)
+				}
 			}else{
 				print("size must be less than 5KB")
 				serverResponse := Response {0, true}
 				println("Server Response:", serverResponse.id, serverResponse.flag)
 				temp.Execute(w,serverResponse)
-			}
-
-			
-			
-		 
+			} 
 
 	}
 
@@ -74,17 +87,43 @@ func Dump(w http.ResponseWriter, r *http.Request){
 	temp.Execute(w,"Dump")
 }
 
-func UploadFiles(r *http.Request)(string){
+func UploadFiles(r *http.Request)(*os.File){
 	r.ParseMultipartForm(10 << 50)
 			file , handler, err := r.FormFile("fileSeq"); if err != nil{
 				fmt.Println("Error failed.... retry",err)
-				return handler.Filename
+				return nil
 			}
 			defer file.Close()
 				if(handler.Size <= (50 * 1024)){
 					fmt.Println("File name:" + handler.Filename)
-					return handler.Filename
+					if _, err := os.Stat(handler.Filename);os.IsExist(err){
+						fmt.Println("FILE exist ", err)
+					}
+					upldFile , err := ioutil.TempFile("user_data", handler.Filename+"-*.txt"); if err != nil{
+					fmt.Println("Error received!", err)
 				}
-				return "blabla"
-			
+				defer upldFile.Close()
+				// file convert into bytes
+				bytesFile , err := ioutil.ReadAll(file); if err != nil{
+					fmt.Println("Error received!", err)
+				}
+				
+				upldFile.Write(bytesFile)
+				fmt.Println("File added on server")
+					return upldFile
+				}
+				return nil
 }
+
+func FileReadFromDisk(){
+	f , err := os.OpenFile("Covid-19.txt", os.O_RDWR | os.O_CREATE, 0755); if err != nil{
+		println("FILE Open Error ... " , err)
+	}
+	print("File Exist...", f)
+	finfo, err := f.Stat() ; if err != nil{
+		println("File Info not found" , err)
+	}
+	println("File Info" , finfo.Name())
+}
+
+
