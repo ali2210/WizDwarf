70a33973484ad3d4321ebf39c457b77ed730275b
Ali2210 <alideveloper95@gmail.com>
2020-04-03 21:17:24 +0500

    register form commit

--- main.go
+++ main.go
@@ -17,6 +17,23 @@ import(
 		flag bool
 	}
 
+	type Create_User struct{
+		name string
+		fname string
+		sir bool
+		madam bool 
+		address string   // World Coodinates
+		address2 string  // local coodinates
+		zip string
+		city string
+		country string
+		check_me_out bool
+		email string
+		password string
+	}
+
+	
+
 func main(){
 
 	routing := mux.NewRouter()
@@ -48,15 +65,30 @@ func Home(w http.ResponseWriter, r *http.Request){
 			file := UploadFiles(r); if file != nil{
 				println(file) // user file upload
 				choose := r.FormValue("choose")
-				print("choose I make:", choose)
+				println("choose I make:", choose)
 				switch choose{
 				case "0":
 					fmt.Fprintf(w, "Please choose any option ...")
+					temFile := template.Must(template.ParseFiles("index.html"))
+					temFile.Execute(w,"Home")
 				case "1":
 					var name string = "Covid-19"
 					 FileReadFromDisk(name);
-
-					
+				case "2":
+					var name string = "FlaviDengue"
+					 FileReadFromDisk(name);
+				case "3":
+					var name string = "KenyaEbola"
+					 FileReadFromDisk(name);
+				case "4":
+					var name string = "ZikaVirusBrazil"
+					 FileReadFromDisk(name);
+				case "5":
+					var name string = "MersSaudiaArabia"
+					 FileReadFromDisk(name);
+				default:
+					temFile := template.Must(template.ParseFiles("index.html"))
+					temFile.Execute(w,"Home")
 				}
 			}else{
 				print("size must be less than 5KB")
@@ -71,7 +103,48 @@ func Home(w http.ResponseWriter, r *http.Request){
 
 func NewUser (w http.ResponseWriter, r *http.Request){
 	temp := template.Must(template.ParseFiles("register.html"))
-	temp.Execute(w,"Regsiter")
+	user := Create_User{}
+		if r.Method  ==  "GET"{
+			fmt.Println("Method:" + r.Method)
+			temp.Execute(w,"Regsiter")
+		}else{
+			r.ParseForm()
+			fmt.Println("Url:", r.URL.Path)
+			fmt.Println("Method:" + r.Method)
+			user.name = r.FormValue("uname")
+			user.fname = r.FormValue("ufname")
+			user.address = r.FormValue("address")
+			user.address2 = r.FormValue("address2")
+			user.city = r.FormValue("city")
+			user.country = r.FormValue("country")
+			user.zip = r.FormValue("zip")
+			user.email = r.FormValue("email")
+			user.password = r.FormValue("password")
+			if r.FormValue("sir") == "on"{
+				user.sir = true 
+			}else if r.FormValue("madam") == "on"{
+				user.madam = true
+			} else{
+				fmt.Fprintf(w,"Select any option")
+				user.sir = false
+				user.madam = false
+				temp.Execute(w,"Regsiter")
+			}
+			
+
+			println("Gender:" , user.sir)
+			println("Gender2:", user.madam)
+			if r.FormValue("check") == "on"{
+					user.check_me_out = true
+			}else{
+				user.check_me_out = false
+			}
+			
+			println("check:" , user.check_me_out)
+			println("User record:" , user.name, user.email)
+			temp.Execute(w,"Regsiter")
+		}
+
 }
 
 func Existing(w http.ResponseWriter, r *http.Request){
@@ -117,7 +190,7 @@ func FileReadFromDisk(filename string){
 	f , err := os.OpenFile(filename + ".txt", os.O_RDWR | os.O_CREATE, 0755); if err != nil{
 		println("FILE Open Error ... " , err)
 	}
-	print("File Exist...", f)
+	println("File Exist...", f)
 	finfo, err := f.Stat() ; if err != nil{
 		println("File Info not found" , err)
 	}
