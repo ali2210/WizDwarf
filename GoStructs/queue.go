c824939a31f0ab63bb4b9ece57f869d75b28c899
Ali2210 <alideveloper95@gmail.com>
2020-04-20 16:38:09 +0500

    changes

--- GoStructs/queue.go
+++ GoStructs/queue.go
@@ -35,27 +35,22 @@ func (q QueueList)DeQueue()(string){
 		q.Length = q.Length -1 
 		println("Length [this]:", q.Length)
 	}else{
-		q.Length = q.Length -1
+		// q.Length = q.Length -1
+		q.Length = q.Recursion(q.Length)
 		println("this Length:", q.Length)
 	}
 	return q.ELement
 
 }
 
-func (q QueueList)DequeueChunk(){
-	myqueue := []QueueList{}
-	len:=0
-	if q.isCapacityFull(q.Length+1){
-		println("Queue Capacity full")
-	}else{
-		println("Queue have some thing", q.Length)
-		myqueue[len].ELement = q.ELement
-		 println(myqueue[len].ELement)
-		len = len + 1
-		println("Queue:",len)
-	}	
-		 
+func (q QueueList)Recursion(int)int{
+	if q.Length == 0{
+		return 0
+	}
+	return q.Recursion(q.Length-1)
+
 }
+
 
 func (q QueueList)Print(){
 	println("Data print:", q.ELement)
