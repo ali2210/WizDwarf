a437f9b877ba7756a2ef0d31722483bde6523fc7
Ali2210 <alideveloper95@gmail.com>
2020-04-29 14:43:09 +0500

    get visitor -2

--- db/firestore.go
+++ db/firestore.go
@@ -70,9 +70,10 @@ func (*cloud_data)FindAllData(app *firebase.App)([]Vistors,error){
 
 	var visits []Vistors
 	iterator := client.Collection(collectionName).Documents(ctx)
-	fmt.Printf("Iterator:%s\n", iterator)
+	fmt.Printf("Iterator:%+v\n", iterator)
+	defer iterator.Stop()
 	for{
-		doc, err := iterator.Next(); if err != nil{
+		doc, err := iterator.Next();if err != nil{
 			log.Fatal("Iterator Failed on Vistor: ", err)
 			return nil, err
 		}
