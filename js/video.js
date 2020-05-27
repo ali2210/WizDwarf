abfa8de35b09abaf8079c2924241202711dcf22f
Ali2210 <alideveloper95@gmail.com>
2020-05-27 13:49:52 +0500

    webrtc promise

--- js/video.js
+++ js/video.js
@@ -2,33 +2,26 @@
 
 // global variables 
 var video = null;
-var canvas = null;
+// var canvas = null;
 
 
 
 		video = document.getElementById('video');
-		canvas = document.getElementById('canvas');
-		audio = document.getElementById('audio');
+		// canvas = document.getElementById('canvas');
+
+let stream =  await navigator.mediaDevices.getUserMedia({video: true, audio :false});
+let rec =  new RecordRTCPromisesHandler(stream, {
+	type : video,
+});
+
+rec .startRecording();
+
+const sleep = m => new Promise(r => setTimeout(r, m));
+await sleep(3000);
 
 
-		var emulateDevice =  navigator.mediaDevices.enumerateDevices().then(function(device) {
-			
-			// camera
-			var cam = device.find(function(device){
-				return device.kind === "video";
-			});
+await rec.stopRecording();
+let blob = await rec.getBlob();
+invokeSaveAsDialog(blob); 
 
-			var mic = device.find(function(device){
-				return device.kind === "audio";
-			})
-
-			var constraints = {video:cam && mediaConstraints.video, audio:mic && mediaConstraints.audio};
-			console.log("getUserMedia= :" + JSON.stringify(constraints));
-			return navigator.mediaDevices.getUserMedia(constraints)
-				.then(function(stream) {
-					getUserMedia(stream);
-				}).catch(function(err) {
-					console.log(err);
-				});
-
-		});
+		
