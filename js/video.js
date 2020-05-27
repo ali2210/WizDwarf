1c537c1e5a6ba8841f4fb9ced49223e8a6b56dea
Ali2210 <alideveloper95@gmail.com>
2020-05-27 12:11:30 +0500

    webcaam access or not

--- js/video.js
+++ js/video.js
@@ -1,53 +1,23 @@
-(function() {
-	var width = 320; 
-	var height = 0;																																																																																																																																																																																																																																																						
-    var	streaming = false;
-    var video = null;
-    var canvas = null;
 
 
-function startup() {
-	// body...
-	video = document.getElementById('video');
-	canvas = document.getElementById('canvas');
+// global variables 
+var video = null;
+var canvas = null;
 
 
-	// navigator 
-	navigator.mediaDevices.getUserMedia({video: true, audio :false})
-		.then(function(stream){
 
-			console.log("stream:", stream);
-			// video source intialize...
-			video.srcObject = stream;
+		video = document.getElementById('video');
+		canvas = document.getElementById('canvas');
 
-			// video start play
-			video.play();
-		})
-		.catch(function(err){
 
-			// console output
-			console.log("Error:" + err)
-		});
-
-		video.addEventListener('canplay', function(ev){
-			if (!streaming) {
-				height = video.videoHeight / (video.videoWidth/width);
-
-				if(isNaN(height)){
-					height = width/(4/3);
-				}
-
-				video.setAttribute('width',width);
-				video.setAttribute('height',height);
-				canvas.setAttribute('width', width);
-				canvas.setAttribute('height',height);
-				streaming = true;
-			}
-
-		}, false);
-
-	}
-
-	window.addEventListener('load',startup,false);
-})
-
+		if (navigator.mediaDevices.getUserMedia){
+			navigator.mediaDevices.getUserMedia({video:true, audio:false})
+			.then(function (stream) {
+				// body...
+				video.srcObject = stream;
+				video.play();
+			})
+			.catch(function(err){
+				console.log("Error:"+ err)
+			})
+		}
