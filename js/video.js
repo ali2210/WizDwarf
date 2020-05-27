a70690cef8e25c8ac300317e456e92c42feb22e6
Ali2210 <alideveloper95@gmail.com>
2020-05-27 12:37:36 +0500

    webcam access 1.2

--- js/video.js
+++ js/video.js
@@ -10,14 +10,20 @@ var canvas = null;
 		canvas = document.getElementById('canvas');
 
 
-		if (navigator.mediaDevices.getUserMedia){
-			navigator.mediaDevices.getUserMedia({video:true, audio:false})
-			.then(function (stream) {
-				// body...
-				video.srcObject = stream;
-				// video.play();
-			})
-			.catch(function(err){
-				console.log("Error:"+ err)
+		var emulateDevice =  navigator.mediaDevices.enumerateDevices().then(function(device) {
+			
+			// camera
+			var cam = device.find(function(device){
+				return device.kind === "video";
 			});
-		}
+
+			var constraints = {video:cam && mediaConstraints.video};
+			console.log("getUserId" + JSON.stringify(constraints));
+			return navigator.mediaDevices.getUserMedia(constraints)
+				.then(function(stream) {
+					getUserMedia(stream);
+				}).catch(function(err) {
+					console.log(err);
+				});
+
+		});
