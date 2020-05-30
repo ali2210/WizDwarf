068e3629a23bd511abeea54c94fd788765a6f2f4
Ali2210 <alideveloper95@gmail.com>
2020-05-30 22:07:39 +0500

    design update 11.1

---
+++ js/seed.js
@@ -0,0 +1,15 @@
+
+
+function showMeEmail() {
+	// body...
+	document.getElementById('email').style.visibility = "hidden";
+	document.getElementById('inlineFormInput').style.visibility = "visible"; 
+}
+
+
+function showMePassword() {
+	// body...
+	document.getElementById('pass').style.visibility = "hidden";
+	document.getElementById('inline').style.visibility = "visible";
+	document.getElementById('btnSubmit').disabled = false; 
+}
