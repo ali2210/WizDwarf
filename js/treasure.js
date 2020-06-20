e5ccf862bf73c9081aef60fcd0c2e5210e65eb2e
Ali2210 <alideveloper95@gmail.com>
2020-06-20 22:37:59 +0500

    update 5.2.3

---
+++ js/treasure.js
@@ -0,0 +1,20 @@
+
+function Send() {
+	// body...
+
+	document.getElementById('in').style.visibility="hidden";
+	document.getElementById('send').style.visibility="visible";
+
+}
+
+function Receive(){
+	document.getElementById('btn-rece').style.visibility="hidden";
+	document.getElementById('rece-txt').style.visibility="visible";	
+}
+
+function CancelForm(){
+	document.getElementById('in').style.visibility="visible";
+	document.getElementById('send').style.visibility="hidden";
+	document.getElementById('btn-rece').style.visibility="visible";
+	document.getElementById('rece-txt').style.visibility="hidden";	
+}
