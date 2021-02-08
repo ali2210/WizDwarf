f7eed803cd697e4a70489c409e58ceec3890ada6
Ali2210 <AliMatrixCode@protonmail.com>
2021-02-08 17:24:36 +0500

    push another ux

--- js/profile.js
+++ js/profile.js
@@ -1,84 +1,91 @@
 
-function EditEvent(){
+function EditEvent() {
     var submit = document.getElementById('submBtn');
     console.log(submit);
-    submit.style.visibility = "visible";    
-    
+    submit.style.visibility = "visible";
+
 }
 
 
-function EmailEditEvent(){
-    var email = document.getElementById('email'); 
+function EmailEditEvent() {
+    var email = document.getElementById('email');
     var duplicate = document.getElementById('demail');
     email.remove();
     duplicate.style.visibility = "visible";
     var button = document.getElementById('button-input');
     button.style.marginTop = '37px';
-        EditEvent();
-
+    EditEvent();
 }
 
-
-
-function NameEditEvent(){
-    var name_in = document.getElementById('name'); 
+function NameEditEvent() {
+    var name_in = document.getElementById('name');
     var duplicate = document.getElementById('duname');
     name_in.remove();
     duplicate.style.visibility = "visible";
     var button = document.getElementById('button-input-name');
     button.style.marginTop = '37px';
-        EditEvent();
-    
+    EditEvent();
+
 }
 
-function FamilyNameEditEvent(){
-    var fname_in = document.getElementById('fname'); 
+function FamilyNameEditEvent() {
+    var fname_in = document.getElementById('fname');
     var duplicate = document.getElementById('dufname');
     fname_in.remove();
     duplicate.style.visibility = "visible";
     var button = document.getElementById('button-input-fname');
     button.style.marginTop = '37px';
-        EditEvent();
+    EditEvent();
 }
 
-function ResidenceEditEvent(){
+function ResidenceEditEvent() {
     var res_in = document.getElementById('inputAddress');
     var duplicate = document.getElementById('daddr');
     res_in.remove();
     duplicate.style.visibility = "visible";
     var button = document.getElementById('button-input-addr');
     button.style.marginTop = '37px';
-        EditEvent();
+    EditEvent();
 }
 
-function SubResEditEvent(){
+function SubResEditEvent() {
     var sres_in = document.getElementById('inputAddress2');
     var duplicate = document.getElementById('dadd');
     sres_in.remove();
     duplicate.style.visibility = "visible";
     var button = document.getElementById('button-input-local');
     button.style.marginTop = '37px';
-        EditEvent();
+    EditEvent();
 }
 
-function CountryEditEvent(){
+function CountryEditEvent() {
     var coun_in = document.getElementById('country');
     var duplicate = document.getElementById('dcoun');
     coun_in.remove();
     duplicate.style.visibility = "visible";
     var button = document.getElementById('button-input-country');
     button.style.marginTop = '37px';
-        EditEvent();
+    EditEvent();
 }
 
 
 
-function ZipEditEvent(){
-    var zip_in = document.getElementById('inputZip'); 
+function ZipEditEvent() {
+    var zip_in = document.getElementById('inputZip');
     var duplicate = document.getElementById('dzip');
     zip_in.remove();
     duplicate.style.visibility = "visible";
     var button = document.getElementById('button-input-zip');
     button.style.marginTop = '37px';
-        EditEvent();
+    EditEvent();
+}
+
+function TweetEditEvent() {
+    var tweet = document.getElementById('tweet');
+    var duplicate = document.getElementById('dtweet');
+    tweet.remove();
+    duplicate.style.visibility = "visible";
+    var button = document.getElementById('button-twitter');
+    button.style.marginTop = '37px';
+    EditEvent();
 }
