fa241872f705da790171070ff89ed90a34829e6c
Ali2210 <AliMatrixCode@protonmail.com>
2021-03-03 23:52:20 +0500

    critical bug update === booooohhhhh :) :p

--- structs/htmlParse.go
+++ structs/htmlParse.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -19,12 +20,13 @@ type (
 
 	HtmlContentInterface interface {
 		ReadContent(pageURL string) ([]HtmlContent, error)
+		SplitContent(content HtmlContent) []HtmlContent
 	}
 
 	ParserObject struct{}
 )
 
-func (*ParserObject) ReadContent(pageURL string) ([]HtmlContent, error) {
+func (p *ParserObject) ReadContent(pageURL string) ([]HtmlContent, error) {
 
 	file, err := os.Open(pageURL)
 	if err != nil {
@@ -43,19 +45,49 @@ func (*ParserObject) ReadContent(pageURL string) ([]HtmlContent, error) {
 		return []HtmlContent{}, err
 	}
 
-	transactPage := make([]HtmlContent, 0)
-	parse := [4]HtmlContent{}
-	for i := range parse {
-		parse[i].Typeclass = doc.Find("h3").Text()
-		parse[i].Price = doc.Find("i").Text()
-		parse[i].Description = doc.Find("h6").Text()
-		parse[i].Paypal = doc.Find("i").Text()
-		parse[i].Token = doc.Find("i").Text()
-		fmt.Println("@Param:", parse[i])
-		transactPage = append(transactPage, parse[i])
+	transactPage := make([]HtmlContent, 3)
+	parse := HtmlContent{}
+	parse.Typeclass = doc.Find("h3").Text()
+	parse.Price = doc.Find("i").Text()
+	parse.Description = doc.Find("h6").Text()
+	parse.Paypal = doc.Find("i").Text()
+	parse.Token = doc.Find("i").Text()
+	t := p.SplitContent(parse)
+	for i := range transactPage {
+		transactPage = append(transactPage, t[i])
 	}
 
-	fmt.Println("@Param:", transactPage)
+	fmt.Println("@Param_Data:", t)
 
 	return transactPage, nil
+}
+
+func (*ParserObject) SplitContent(content HtmlContent) []HtmlContent {
+	parser := make([]HtmlContent, 3)
+	for i := range parser {
+		if strings.Contains(content.Typeclass, "Kernel") && parser[0].Typeclass == "" {
+			parser[i].Typeclass = "Kernel"
+			parser[i].Price = "$50"
+			parser[i].Description = "2D Molecular structure"
+			parser[i].Paypal = "Paypal"
+			parser[i].Description = "Crypto-Tokens"
+		} else {
+			if strings.Contains(content.Typeclass, "Cluster") && (parser[1].Typeclass == "") {
+				parser[i].Typeclass = "Cluster"
+				parser[i].Price = "$100"
+				parser[i].Description = "3D Molecular structure"
+				parser[i].Paypal = "Paypal"
+				parser[i].Token = "Crypto-Tokens"
+			} else {
+				if strings.Contains(content.Typeclass, "Multi-Cluster") && (parser[2].Typeclass == "") {
+					parser[i].Typeclass = "Multi-Cluster"
+					parser[i].Price = "$500"
+					parser[i].Paypal = "Paypal"
+					parser[i].Token = "Crypto-Tokens"
+					parser[i].Description = "Automated argumented 4D+ Molecular structure analysis."
+				}
+			}
+		}
+	}
+	return parser
 }
