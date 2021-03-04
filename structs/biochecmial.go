9c8ef684cbc2cb3c44b5016053547b4970729b95
Ali2210 <AliMatrixCode@protonmail.com>
2021-03-04 22:19:45 +0500

    bio ===

--- structs/biochecmial.go
+++ structs/biochecmial.go
@@ -5,23 +5,109 @@ import (
 	"github.com/fogleman/ribbon/pdb"
 )
 
-type MolecularBio struct {
-
-	// Helix Object Reference
-	HelixA pdb.Helix
-	// Strand Object Reference
-	StrandB pdb.Strand
-}
+type (
+	MolecularBio struct {
+		AtomLevel  *pdb.Atom
+		AlphaHelix *pdb.Helix
+		BetaSheets *pdb.Strand
+		LinkBy     []*pdb.Connection
+		Chains     *pdb.Chain
+		BioChem    *pdb.Matrix
+		Resuide    *pdb.Residue
+	}
+)
 
 func SequenceStructure(st2 string) MolecularBio {
 
-	molecule := MolecularBio{}
+	molecule := MolecularBio{
+		AtomLevel: &pdb.Atom{
+			Serial:     0,
+			Name:       "",
+			AltLoc:     "",
+			ResName:    "",
+			ChainID:    "",
+			ResSeq:     0,
+			ICode:      "",
+			X:          0,
+			Y:          0,
+			Z:          0,
+			Occupancy:  0,
+			TempFactor: 0,
+			Element:    "",
+			Charge:     "",
+		},
+		AlphaHelix: &pdb.Helix{
+			Serial:      0,
+			HelixID:     "",
+			InitResName: "",
+			InitChainID: "",
+			InitSeqNum:  0,
+			InitICode:   "",
+			EndResName:  "",
+			EndChainID:  "",
+			EndSeqNum:   0,
+			EndICode:    "",
+			HelixClass:  0,
+			Length:      0,
+		},
+		BetaSheets: &pdb.Strand{
+			Strand:      0,
+			SheetID:     "",
+			NumStrands:  0,
+			InitResName: "",
+			InitChainID: "",
+			InitSeqNum:  0,
+			InitICode:   "",
+			EndResName:  "",
+			EndChainID:  "",
+			EndSeqNum:   0,
+			EndICode:    "",
+			Sense:       0,
+			CurAtom:     "",
+			CurResName:  "",
+			CurChainId:  "",
+			CurResSeq:   0,
+			CurICode:    "",
+			PrevAtom:    "",
+			PrevResName: "",
+			PrevChainId: "",
+			PrevResSeq:  0,
+			PrevICode:   "",
+		},
+		LinkBy: []*pdb.Connection{},
+		Chains: &pdb.Chain{
+			ChainID:  "",
+			Residues: []*pdb.Residue{},
+		},
+		BioChem: &pdb.Matrix{},
+		Resuide: &pdb.Residue{
+			ResName:     "",
+			ChainID:     "",
+			ResSeq:      0,
+			Atoms:       []*pdb.Atom{},
+			AtomsByName: map[string]*pdb.Atom{},
+			Type:        0,
+		},
+	}
 
-	hlix := *pdb.ParseHelix(st2)
-	stand := *pdb.ParseStrand(st2)
+	molecule.AlphaHelix = pdb.ParseHelix(st2)
+	molecule.BetaSheets = pdb.ParseStrand(st2)
+	molecule.AtomLevel = pdb.ParseAtom(st2)
+	molecule.BioChem = &pdb.Matrix{}
+	molecule.Chains = &pdb.Chain{
+		ChainID:  "",
+		Residues: []*pdb.Residue{},
+	}
 
-	molecule.HelixA = hlix
-	molecule.StrandB = stand
+	molecule.Resuide = &pdb.Residue{
+		ResName:     "",
+		ChainID:     "",
+		ResSeq:      0,
+		Atoms:       []*pdb.Atom{},
+		AtomsByName: map[string]*pdb.Atom{},
+		Type:        0,
+	}
+	molecule.LinkBy = pdb.ParseConnections(st2)
 
 	return molecule
 }
