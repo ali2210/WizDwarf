a2893668f6a4eeac9e8f10f326bfa0a44c3d6618
Ali2210 <Alideveloper95@gmail.com>
2022-02-18 20:38:01 +0500

    terraform fixed

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "docker" {}
 
-resource "docker_image" "traefik"{
-  name = "traefik:latest"
+resource "docker_image" "traefik" {
+  name         = "traefik:latest"
   keep_locally = true
 }
 
@@ -20,17 +20,17 @@ resource "docker_image" "golang" {
   keep_locally = true
 }
 
-resource "docker_container" "traefik"{
-  name = "traefik"
+resource "docker_container" "traefik" {
+  name  = "traefik"
   image = docker_image.traefik.latest
-  ports{
-    internal = 80 
+  ports {
+    internal = 80
     external = 8080
   }
 }
 
-resource "docker_container" "wizdwarfs"{
-  name = "wizdwarfs"
+resource "docker_container" "wizdwarfs" {
+  name  = "wizdwarfs"
   image = var.golang_dock
   ports {
     internal = 5000
